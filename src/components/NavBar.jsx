7999a1f59889a39a9afd10d892589ba43948ff79
RamanSlinka <romuch1986@gmail.com>
2021-08-27 16:05:00 +0300

    add NavBar and Auth components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import {SHOP_ROUTE} from "../utils/constants";
+import {observer} from "mobx-react-lite";
+
+
+const NavBar = observer(() => {
+        const {user} = useContext(Context)
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <NavLink  style={{color: 'white'}} to={SHOP_ROUTE}>My First Shop</NavLink>
+                    {user.isAuth
+                        ?
+                        <Nav className="ml-auto" style={{color: 'white'}}>
+                            <Button variant={"outline-light"}>Admin</Button>
+                            <Button variant={"outline-light"} className="ml-4">Sign in</Button>
+                        </Nav>
+                        :
+                        <Nav className="ml-auto" style={{color: 'white'}}>
+                            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Authorization</Button>
+                        </Nav>
+                    }
+                </Container>
+            </Navbar>
+        );
+    }
+)
+export default NavBar;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/constants";
 
 const Auth = () => {
+
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
-        <div>
-            auth
-        </div>
+        <Container className="d-flex justify-content-center align-items-center"
+                   style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Enter your e-mail..."
+                    />
+
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Enter your password..."
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите</NavLink>
+                            </div>
+                        }
+                        <Button
+                            className="mt-3 "
+                            style={{width: "80px"}}
+                            size="sm"
+                            variant="outline-success"
+                        >
+                            {isLogin ? "Войти" : "Регистрациия"}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
