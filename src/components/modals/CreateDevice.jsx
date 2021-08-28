fac3bb67fe35f1163510051a2acb36b973677a05
RamanSlinka <romuch1986@gmail.com>
2021-08-28 21:33:10 +0300

    ***

--- src/components/modals/CreateDevice.jsx
+++ src/components/modals/CreateDevice.jsx
@@ -8,10 +8,10 @@ const CreateDevice = ({show, onHide}) => {
     const [info, setInfo] = useState([])
 
     const addInfo = () => {
-        setInfo( [...info, {title: '', description: '', number: Date.now()}])
+        setInfo([...info, {title: '', description: '', number: Date.now()}])
     }
     const removeInfo = (number) => {
-        setInfo( info.filter(i => i.number !== number))
+        setInfo(info.filter(i => i.number !== number))
     }
 
     return (
@@ -27,14 +27,14 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                   <Dropdown className="mt-2 mb-2">
-                       <Dropdown.Toggle>Select type</Dropdown.Toggle>
-                       <Dropdown.Menu>
-                           {device.types.map(type =>
-                               <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
-                           )}
-                       </Dropdown.Menu>
-                   </Dropdown>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Select type</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.types.map(type =>
+                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                     <Dropdown className="mt-2">
                         <Dropdown.Toggle>Select brand</Dropdown.Toggle>
                         <Dropdown.Menu>
@@ -44,50 +44,52 @@ const CreateDevice = ({show, onHide}) => {
                         </Dropdown.Menu>
                     </Dropdown>
 
+
                     <Form.Control
                         className="mt-3"
                         placeholder="Enter device name "
-                        />
+                    />
                     <Form.Control
                         className="mt-3"
                         placeholder="Enter device price "
                         type="number"
-                        />
+                    />
                     <Form.Control
                         className="mt-3"
-                       type="file"
-                        />
-                    <hr/>
+                        type="file"
+                    />
+
+
                     <Button
-                    variant={"outline-dark"}
-                    onClick={addInfo}
+                        className="mt-3"
+                        variant={"outline-dark"}
+                        onClick={addInfo}
                     >
                         Add new property
                     </Button>
 
+
                     {info.map(i =>
-                    <Row className="mt-2" key={i.number}>
-                        <Col md={4}>
-                            <Form.Control
-
-                                placeholder="Enter property name "
+                        <Row className="mt-2" key={i.number}>
+                            <Col md={4}>
+                                <Form.Control
+                                    placeholder="Enter property name "
                                 />
-                        </Col>
-                        <Col md={4}>
-                            <Form.Control
-
-                                placeholder="Enter property description "
+                            </Col>
+                            <Col md={4}>
+                                <Form.Control
+                                    placeholder="Enter property description "
                                 />
-                        </Col>
-                        <Col md={4}>
-                            <Button
-                                onClick={()=> removeInfo(i.number)}
-                                variant={"outline-danger"}>
-                                Delete
-                            </Button>
-                        </Col>
-                    </Row>
-                        )}
+                            </Col>
+                            <Col md={4}>
+                                <Button
+                                    onClick={() => removeInfo(i.number)}
+                                    variant={"outline-danger"}>
+                                    Delete
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
                 </Form>
             </Modal.Body>
             <Modal.Footer>
